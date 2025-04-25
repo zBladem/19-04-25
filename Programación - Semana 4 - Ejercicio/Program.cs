0b79770a670837c0400ab094c78730f10c6d2bc6
Jean Flores <jeanpierre194fp@gmail.com>
2025-04-24 22:06:54 -0500

    VisualStudio

---
+++ Programación - Semana 4 - Ejercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programación___Semana_4___Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
