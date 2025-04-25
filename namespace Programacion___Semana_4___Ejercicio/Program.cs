bd4fdb6ce2d91794d53617ef4231ae659b8e6d76
Jean Flores <jeanpierre194fp@gmail.com>
2025-04-24 22:17:37 -0500

    CreacionDelVisualStudio

---
+++ namespace Programacion___Semana_4___Ejercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespace_Programacion___Semana_4___Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
